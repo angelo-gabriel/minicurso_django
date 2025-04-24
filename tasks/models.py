9446aa38ce3bd787da887c0d3f7c07681b956f8e
Angelo Assis <aangelo6974@gmail.com>
2025-04-24 19:39:51 -0300

    add views

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    completed = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['-created_date']  # Ordenar por data de criação, mais recentes primeiro
